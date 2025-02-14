18e4b5b12252fd66287e1e3ae427afb1104ce0c6
edoubart <edouard.barthelemy@gmail.com>
2025-02-14 11:17:30 -0500

    Add Attributes and Inherent Implementations for structs, and enrich main function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+/***********
+ * Structs *
+ ***********/
+
+// Attributes
 #[derive(Debug)]
 struct Account {
     id: u32,
@@ -5,11 +10,39 @@ struct Account {
     balance: i32,
 }
 
+// Inherent Implementations
+impl Account {
+    // Associated Functions
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+// Attributes
 #[derive(Debug)]
 struct Bank {
     accounts: Vec<Account>,
 }
 
+// Inherent Implementations
+impl Bank {
+    // Associated Functions
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account: Account) {
+    println!("{:#?}", account);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let account = Account::new(1, String::from("me"));
+
+    print_account(account);
 }
