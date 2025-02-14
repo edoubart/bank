a92723d7c838710211b369663e2e0ecb22f761b4
edoubart <edouard.barthelemy@gmail.com>
2025-02-14 10:59:11 -0500

    Add Bank and Account structs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    holder: String,
+    balance: i32,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
