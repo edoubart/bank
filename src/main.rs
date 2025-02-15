46653f6572b99ee951d6901f53d63835eeb94938
edoubart <edouard.barthelemy@gmail.com>
2025-02-15 01:04:09 -0500

    Working version with Borrowing (passing of immutable reference to 'account' in that case).

--- src/main.rs
+++ src/main.rs
@@ -36,17 +36,14 @@ impl Bank {
     }
 }
 
-fn print_account(account: Account) -> Account {
+fn print_account(account: &Account) {
     println!("{:#?}", account);
-    account
 }
 
 fn main() {
-    let mut account = Account::new(1, String::from("me"));
+    let account = Account::new(1, String::from("me"));
 
-    account = print_account(account);
-
-    account = print_account(account);
+    print_account(&account);
 
     println!("{:#?}", account);
 }
